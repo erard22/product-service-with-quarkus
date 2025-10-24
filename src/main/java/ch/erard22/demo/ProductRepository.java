f32a46e4d325964d47f9488320490d5c08ff25e4
michel <michel.erard@gmail.com>
2025-10-24 12:03:26 +0200

    panache repository

--- src/main/java/ch/erard22/demo/ProductRepository.java
+++ src/main/java/ch/erard22/demo/ProductRepository.java
@@ -1,35 +1,9 @@
 package ch.erard22.demo;
 
-import java.util.List;
-
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
 
 @ApplicationScoped
-public class ProductRepository {
+public class ProductRepository implements PanacheRepositoryBase<Product, Long> {
 
-  @Inject
-  EntityManager em;
-
-  public List<Product> searchByName(String name) {
-    TypedQuery<Product> query = em.createQuery(
-        "SELECT p FROM Product p WHERE p.name = :name", Product.class);
-    query.setParameter("name", name);
-    return query.getResultList();
-  }
-
-  public Product getById(long id) {
-    return em.find(Product.class, id);
-  }
-
-  public Product save(Product product) {
-    em.persist(product);
-    return product;
-  }
-
-  public List<Product> listAll() {
-    return em.createQuery("SELECT p FROM Product p", Product.class).getResultList();
-  }
 }

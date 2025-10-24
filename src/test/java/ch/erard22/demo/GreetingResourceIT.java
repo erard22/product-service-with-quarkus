d7ab2889143475b42a62c69ecf4c45018a2eca95
michel <michel.erard@gmail.com>
2025-10-24 11:38:37 +0200

    Initial version

---
+++ src/test/java/ch/erard22/demo/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package ch.erard22.demo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
